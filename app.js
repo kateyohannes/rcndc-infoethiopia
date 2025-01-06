3d066e68f5808dc71a12e60a54de782b6237aba8
Yohannes Teshome <kateyohannes@gmail.com>
2025-01-06 11:44:47 -0500

    server init

---
+++ app.js
@@ -0,0 +1,19 @@
+require("./config/mongoose")();
+const http = require("node:http")
+const express = require("express")
+const catagoryRoute = require("./routes/catagory.route")
+
+const app = express()
+
+app.use(require("morgan")("dev"))
+app.use(express.json())
+
+app.use("/catagory", catagoryRoute);
+
+const server = http.createServer(app)
+const port = 3000
+server.listen(port, ()=>{
+    console.log(`Server is on port : ${port}`)
+})
+
+module.exports = app;
