c78b3a9459c472da3d4456d22351c72865e2efc1
Yohannes Teshome <kateyohannes@gmail.com>
2025-01-07 14:16:30 -0500

    catagory validator

---
+++ validator/catagory.validator.js
@@ -0,0 +1,31 @@
+
+const Joi = require("joi");
+
+module.exports = {
+    addCatagoryValidator: async (req, res, next)=>{
+        const schema = Joi.object({
+            name: Joi.string().required(),
+            parent: Joi.string().optional()
+        })
+
+        const { error } = schema.validate(req.body);
+        if(error){
+            return res.status(400).json(error)
+        }
+
+        next()
+    },
+    updateCatagoryValidator: async (req, res, next)=>{
+        const schema = Joi.object({
+            name: Joi.string().optional(),
+            parent: Joi.string().optional()
+        })
+
+        const { error } = schema.validate(req.body);
+        if(error){
+            return res.status(400).json(error)
+        }
+
+        next()
+    }
+}
