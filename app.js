2d56c10a3e4a4b630b52a02ac53a0991241efc87
Yohannes Teshome <kateyohannes@gmail.com>
2025-01-08 16:13:37 -0500

    add more grouped route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("./config/mongoose")();
 const http = require("node:http")
 const express = require("express")
 const catagoryRoute = require("./routes/catagory.route")
+const userRoute = require("./routes/user.route")
 
 const app = express()
 
@@ -9,11 +10,12 @@ app.use(require("morgan")("dev"))
 app.use(express.json())
 
 app.use("/catagory", catagoryRoute);
+app.use("/user", userRoute)
 
 const server = http.createServer(app)
 const port = 3000
 server.listen(port, ()=>{
     console.log(`Server is on port : ${port}`)
-})
+});
 
 module.exports = app;
