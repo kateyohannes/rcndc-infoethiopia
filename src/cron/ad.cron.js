bd0780a72643d1384eff0f73e587b96f02d00df7
Yohannes Teshome <kateyohannes@gmail.com>
2025-02-07 09:27:48 -0500

    ad cron runs every 24 hours

---
+++ src/cron/ad.cron.js
@@ -0,0 +1,21 @@
+const { CronJob } = require('cron');
+const vc = require("../utils/promise");
+
+module.exports = async (fastify, options)=>{
+    const adCollection = fastify.mongo.db.collection("ad");
+    new CronJob(
+        '0 0 * * *', // every 24 hours at 0
+        async function () {
+            // it revalidate both start and end date of an ad
+            const [ err, start ] = await vc(await adCollection.find({
+                $and: [
+                    { is_checked: true },
+                    { is_visible: false },
+                    { is_approved: { statis: true }}
+                ]
+            }).toArray());
+            if (err) console.log({ err })
+            console.log({ start });
+        },
+    ).start();
+}
