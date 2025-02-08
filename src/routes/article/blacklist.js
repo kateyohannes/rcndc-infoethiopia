6ca03af0c6cda1d4b5889bb872143fd420f02fa8
Yohannes Teshome <kateyohannes@gmail.com>
2025-02-08 10:09:09 -0500

    blacklist GET route

--- src/routes/article/blacklist.js
+++ src/routes/article/blacklist.js
@@ -1,13 +1,15 @@
 'use strict';
 
 module.exports = async (fastify, options)=>{
+    const blacklistColletion = fastify.mongo.db.collection("blacklist");
+    
     fastify.route({
         url: "/",
         method: "GET",
         handler: async (request, reply)=>{
-            return reply.code(200).send({
-                messagE: "blacklisted!"
-            })
+            const [ err, data ] = await vc(await blacklistColletion.find({}).toArray());
+            if (err) return reply.code(500).send(err);
+            return reply.code(200).send(data);
         }
     });
     
